31d5cd79897b39bc722246df783857b784d6c2fb
Thomas Hahn <thomas.hahn01@gmail.com>
2023-10-07 22:25:59 -0400

    Add fmtlib to deps

--- tests/tmp_test.cpp
+++ tests/tmp_test.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include <fmt/ranges.h>
+#include <vector>
+#include <span>
+#include <ranges>
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+    std::vector<int> v { 1, 2, 3 };
+    fmt::print("{}\n", std::span(v));
+    fmt::print("{}\n", std::ranges::iota_view{1, 10});
 }
