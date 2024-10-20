3d9ba68dc3f3e29a51e73ff150c80a8848b1e51a
Thomas Hahn <thomas.hahn01@gmail.com>
2024-10-20 08:10:37 -0400

    Add preliminary docs for jackknife

---
+++ include/simplemc/accs/jackknife.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * @brief Jackknife resampling.
+ */
+
+#ifndef SIMPLEMC_ACCS_JACKKNIFE_HPP
+#define SIMPLEMC_ACCS_JACKKNIFE_HPP
+
+namespace simplemc {
+
+/**
+ * @addtogroup simplemc-accs-resampling
+ * @{
+ */
+
+/**
+ * @brief Class to represent jackknife samples \f$ \mathbf{z}_{[i]}^{(N-1)} \f$ and \f$
+ * f_{[i]}^{(N-1)} \f$.
+ */
+class jackknife_data {};
+
+
+/** @} */
+
+} // namespace simplemc
+
+#endif // SIMPLEMC_ACCS_JACKKNIFE_HPP
