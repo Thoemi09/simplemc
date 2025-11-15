d84dbc66bffd82b358659d1053c487a1d41e127b
Thomas Hahn <thomas.hahn01@gmail.com>
2025-11-15 09:55:43 -0500

    Add a grid_iterator class

---
+++ include/simplemc/grids/grid_iterator.hpp
@@ -0,0 +1,324 @@
+/**
+ * @file
+ * @brief Iterator for grid types.
+ */
+
+#ifndef SIMPLEMC_GRIDS_GRID_ITERATOR_HPP
+#define SIMPLEMC_GRIDS_GRID_ITERATOR_HPP
+
+#include <simplemc/grids/concepts.hpp>
+#include <simplemc/utils/nd_indexing.hpp>
+
+#include <cassert>
+#include <cstddef>
+#include <concepts>
+#include <iterator>
+
+namespace simplemc {
+
+/**
+ * @addtogroup simplemc-grids
+ * @{
+ */
+
+/**
+ * @brief CRTP iterator base class for 1- and N-dimensional grid types.
+ *
+ * @details This class provides random access iteration for all grid types satisfying the
+ * simplemc::grid_common concept.
+ *
+ * It stores the current index \f$ i \f$ (or flat index \f$ i_{\mathrm{lin}} \f$ for N-dimensional
+ * grids), a pointer to the grid object being iterated over and the size of the gird. That means the
+ * underlying grid must outlive the iterator.
+ *
+ * Dereferencing the iterator calls the grid's `at` method with the current index. For N-dimensional
+ * grids, the index is first converted to a multi-dimensional index array using simplemc::nd_index.
+ *
+ * The iterator is considered out of bounds when \f$ i < 0 \f$ or \f$ i \geq M \f$. In this case,
+ * dereferencing the iterator results in undefined behavior.
+ *
+ * @note For N-dimensional grids, the current implementation calls simplemc::nd_index on each
+ * dereference. See simplemc::grid_view for an alternative approach using cartesian product and
+ * transformed views.
+ *
+ * @tparam G Grid type.
+ */
+template <grid_common G>
+class grid_iterator {
+public:
+    /**
+     * @brief Value type (same as the value type of the grid).
+     */
+    using value_type = G::value_type;
+
+    /**
+     * @brief Difference type for iterator arithmetic.
+     */
+    using difference_type = std::ptrdiff_t;
+
+    /**
+     * @brief Reference type.
+     */
+    using reference = value_type;
+
+    /**
+     * @brief Iterator category.
+     */
+    using iterator_category = std::random_access_iterator_tag;
+
+    /**
+     * @brief Grid type.
+     */
+    using grid_type = G;
+
+    /**
+     * @brief Number of dimensions of the underlying grid.
+     */
+    static constexpr std::size_t dim() noexcept { return G::dim(); }
+
+    /**
+     * @brief Default constructor creates an invalid iterator with no associated grid.
+     */
+    constexpr grid_iterator() noexcept = default;
+
+    /**
+     * @brief Construct an iterator for the given grid at the specified (flat) index.
+     *
+     * @details By default, the index is \f$ i = 0 \f$. For N-dimensional grids, the index corresponds
+     * to the flat index, i.e. \f$ i \equiv i_{\mathrm{lin}} \f$.
+     *
+     * The size is set to \f$ 0 \f$ if the grid pointer is `nullptr`.
+     *
+     * @param grid Grid object to iterate over.
+     * @param idx Index \f$ i \f$.
+     */
+    constexpr grid_iterator(const grid_type& grid, difference_type idx = 0) noexcept :
+        grid_ptr_(&grid),
+        idx_(idx),
+        size_(grid_ptr_ ? grid_ptr_->size() : 0) {}
+
+    /**
+     * @brief Dereference operator to get the grid point \f$ g(i) \f$/\f$ g(\mathbf{i}) \f$ at the
+     * current index.
+     *
+     * @details If the iterator is out of bounds, i.e. \f$ i < 0 \f$ or \f$ i \geq M \f$,
+     * dereferencing is undefined behavior.
+     *
+     * @return Grid point at the current index.
+     */
+    [[nodiscard]] constexpr value_type operator*() const noexcept { return dereference(idx_); }
+
+    /**
+     * @brief Pre-increment operator increases the index \f$ i \f$ by \f$ 1 \f$.
+     *
+     * @return Reference to this iterator after incrementing.
+     */
+    constexpr grid_iterator& operator++() noexcept {
+        ++idx_;
+        return *this;
+    }
+
+    /**
+     * @brief Post-increment operator increases the index \f$ i \f$ by \f$ 1 \f$.
+     *
+     * @details It calls operator++() after making a copy of the current iterator.
+     *
+     * @return Copy of the iterator before incrementing.
+     */
+    constexpr grid_iterator operator++(int) noexcept {
+        auto tmp = *this;
+        ++(*this);
+        return tmp;
+    }
+
+    /**
+     * @brief Pre-decrement operator decreases the index \f$ i \f$ by \f$ 1 \f$.
+     *
+     * @return Reference to this iterator after decrementing.
+     */
+    constexpr grid_iterator& operator--() noexcept {
+        --idx_;
+        return *this;
+    }
+
+    /**
+     * @brief Post-decrement operator decreases the index \f$ i \f$ by \f$ 1 \f$.
+     *
+     * @details It calls operator--() after making a copy of the current iterator.
+     *
+     * @return Copy of the iterator before decrementing.
+     */
+    constexpr grid_iterator operator--(int) noexcept {
+        auto tmp = *this;
+        --(*this);
+        return tmp;
+    }
+
+    /**
+     * @brief Addition assignment operator increases the index \f$ i \f$ by \f$ n \f$.
+     *
+     * @param n Increment value for the index.
+     * @return Reference to this iterator.
+     */
+    constexpr grid_iterator& operator+=(difference_type n) noexcept {
+        idx_ += n;
+        return *this;
+    }
+
+    /**
+     * @brief Subtraction assignment operator decreases the index \f$ i \f$ by \f$ n \f$.
+     *
+     * @param n Decrement value for the index.
+     * @return Reference to this iterator.
+     */
+    constexpr grid_iterator& operator-=(difference_type n) noexcept {
+        idx_ -= n;
+        return *this;
+    }
+
+    /**
+     * @brief Addition operator increases the index \f$ i \f$ by \f$ n \f$.
+     *
+     * @details In contrast to operator+=(difference_type), it makes a copy of the current iterator
+     * and updates the copy.
+     *
+     * @param n Increment value for the index.
+     * @return Copy of this iterator with increased index.
+     */
+    [[nodiscard]] constexpr grid_iterator operator+(difference_type n) const noexcept {
+        auto tmp = *this;
+        tmp += n;
+        return tmp;
+    }
+
+    /**
+     * @brief Addition operator increases the index \f$ i \f$ by \f$ n \f$.
+     *
+     * @details See grid_iterator::operator+ for more information.
+     *
+     * @param n Increment value for the index.
+     * @param it Iterator to advance.
+     * @return Copy of the given iterator with increased index.
+     */
+    [[nodiscard]] friend constexpr grid_iterator operator+(difference_type n, const grid_iterator& it) noexcept {
+        return it + n;
+    }
+
+    /**
+     * @brief Subtraction operator decreases the index \f$ i \f$ by \f$ n \f$.
+     *
+     * @details In contrast to operator-=(difference_type), it makes a copy of the current iterator
+     * and updates the copy.
+     *
+     * @param n Decrement value for the index.
+     * @return Copy of this iterator with decreased index.
+     */
+    [[nodiscard]] constexpr grid_iterator operator-(difference_type n) const noexcept {
+        auto tmp = *this;
+        tmp -= n;
+        return tmp;
+    }
+
+    /**
+     * @brief Subtraction operator between two iterators.
+     *
+     * @details It calculates the difference between the indices of the two iterators.
+     *
+     * Both iterators are assumed to point to the same grid, otherwise the result is undefined.
+     *
+     * @param other Right-hand side iterator.
+     * @return Distance between this iterator and the other.
+     */
+    [[nodiscard]] constexpr difference_type operator-(const grid_iterator& other) const noexcept {
+        assert(grid_ptr_ == other.grid_ptr_);
+        return idx_ - other.idx_;
+    }
+
+    /**
+     * @brief Subscript operator to access the grid point at an offset from the current iterator.
+     *
+     * @details It computes the grid point at \f$ g(i + n) \f$.
+     *
+     * @param n Index offset.
+     * @return Grid point at the index\f$ i + n \f$.
+     */
+    [[nodiscard]] constexpr value_type operator[](difference_type n) const noexcept { return dereference(idx_ + n); }
+
+    /**
+     * @brief Equality comparison operator compares the grid pointer and the indices of the two
+     * iterators.
+     *
+     * @param other Right hand side iterator.
+     * @return True if iterators point to the same grid and have the same index.
+     */
+    [[nodiscard]] constexpr bool operator==(const grid_iterator& other) const noexcept {
+        return grid_ptr_ == other.grid_ptr_ && idx_ == other.idx_;
+    }
+
+    /**
+     * @brief Three-way comparison operator to define an ordering between two iterators.
+     *
+     * @details The ordering is the same as that of their indices using `<=>`.
+     *
+     * @param other Right hand side iterator.
+     * @return Ordering between the two iterators.
+     */
+    [[nodiscard]] constexpr auto operator<=>(const grid_iterator& other) const noexcept {
+        assert(grid_ptr_ == other.grid_ptr_);
+        return idx_ <=> other.idx_;
+    }
+
+    /**
+     * @brief Get the pointer to the underlying grid.
+     *
+     * @return Grid pointer.
+     */
+    [[nodiscard]] constexpr const grid_type* grid_ptr() const noexcept { return grid_ptr_; }
+
+    /**
+     * @brief Get the current flat index \f$ i \equiv i_{\mathrm{lin}} \f$.
+     *
+     * @return Current flat index \f$ i \f$.
+     */
+    [[nodiscard]] constexpr difference_type flat_index() const noexcept { return idx_; }
+
+    /**
+     * @brief Get the current multi-dimensional index \f$ \mathbf{i} \f$.
+     *
+     * @details For 1-dimensional grids, this returns the flat index \f$ i \f$. For N-dimensional
+     * grids, it converts the flat index to a multi-dimensional index array using simplemc::nd_index
+     * with row-major ordering.
+     *
+     * @return Current multi-dimensional index \f$ \mathbf{i} \f$ (or \f$ i \f$ for 1D grids).
+     */
+    [[nodiscard]] constexpr auto nd_index() const noexcept {
+        assert(grid_ptr_ && size_ > 0 && idx_ >= 0 && idx_ < size_);
+        if constexpr (dim() == 1) {
+            return idx_;
+        } else {
+            return simplemc::nd_index(idx_, grid_ptr_->shape(), row_major {});
+        }
+    }
+
+private:
+    // Helper function calls at() method of the underlying grid.
+    constexpr auto dereference(difference_type idx) const noexcept {
+        assert(grid_ptr_ && size_ > 0 && idx >= 0 && idx < size_);
+        if constexpr (dim() == 1) {
+            return grid_ptr_->at(idx);
+        } else {
+            return grid_ptr_->at(simplemc::nd_index(idx, grid_ptr_->shape(), row_major {}));
+        }
+    }
+
+private:
+    const grid_type* grid_ptr_ { nullptr };
+    difference_type idx_ { 0 };
+    long size_ { 0 };
+};
+
+/** @} */
+
+} // namespace simplemc
+
+#endif // SIMPLEMC_GRIDS_GRID_ITERATOR_HPP
---
+++ tests/grids/grids_iterator_test.cpp
@@ -0,0 +1,555 @@
+/**
+ * @file
+ * @brief Unit tests for grid_iterator with minimal dummy grid classes.
+ */
+
+#include "../test_utils.hpp"
+
+#include <simplemc/grids/concepts.hpp>
+#include <simplemc/grids/grid_iterator.hpp>
+#include <simplemc/utils/nd_indexing.hpp>
+
+#include <array>
+#include <cstddef>
+#include <tuple>
+
+namespace {
+
+// Minimal dummy 1D grid for testing grid_iterator.
+//
+// This grid represents a simple linear grid with M points from 0.0 to (M-1) * 1.0.
+// Grid points: [0.0, 1.0, 2.0, ..., (M-1) * 1.0]
+// Bins: [0.0, 1.0), [1.0, 2.0), ..., [(M-2) * 1.0, (M-1) * 1.0]
+class dummy_grid_1d {
+public:
+    using value_type = double;
+    using size_type = long;
+
+    constexpr explicit dummy_grid_1d(long size) noexcept : size_(size) {}
+
+    [[nodiscard]] static constexpr std::size_t dim() noexcept { return 1; }
+
+    [[nodiscard]] constexpr long size() const noexcept { return size_; }
+
+    [[nodiscard]] constexpr double at(long i) const noexcept { return static_cast<double>(i); }
+
+    [[nodiscard]] constexpr long index(double x) const noexcept { return static_cast<long>(x); }
+
+    [[nodiscard]] constexpr double volume(long /* i */) const noexcept { return 1.0; }
+
+    [[nodiscard]] constexpr double center(long i) const noexcept { return static_cast<double>(i) + 0.5; }
+
+private:
+    long size_;
+};
+
+// Minimal dummy 2D grid for testing grid_iterator.
+//
+// This grid represents a 2D grid with shape [M0, M1] where each underlying 1D grid is a
+// dummy_grid_1d. Grid points are multi-dimensional indices converted to double arrays.
+class dummy_grid_2d {
+public:
+    using value_type = std::array<double, 2>;
+    using size_type = std::array<long, 2>;
+    using tuple_type = std::tuple<dummy_grid_1d, dummy_grid_1d>;
+
+    constexpr explicit dummy_grid_2d(size_type shape) noexcept : shape_(shape), grid0_(shape[0]), grid1_(shape[1]) {}
+
+    [[nodiscard]] static constexpr std::size_t dim() noexcept { return 2; }
+
+    [[nodiscard]] constexpr long size() const noexcept { return shape_[0] * shape_[1]; }
+
+    [[nodiscard]] constexpr size_type shape() const noexcept { return shape_; }
+
+    [[nodiscard]] constexpr tuple_type grids() const noexcept { return std::make_tuple(grid0_, grid1_); }
+
+    [[nodiscard]] constexpr value_type at(size_type idx) const noexcept {
+        return { grid0_.at(idx[0]), grid1_.at(idx[1]) };
+    }
+
+    [[nodiscard]] constexpr size_type index(size_type x) const noexcept { return x; }
+
+    [[nodiscard]] constexpr double volume(size_type idx) const noexcept {
+        return grid0_.volume(idx[0]) * grid1_.volume(idx[1]);
+    }
+
+    [[nodiscard]] constexpr value_type center(size_type idx) const noexcept {
+        return { grid0_.center(idx[0]), grid1_.center(idx[1]) };
+    }
+
+private:
+    size_type shape_;
+    dummy_grid_1d grid0_;
+    dummy_grid_1d grid1_;
+};
+
+} // anonymous namespace
+
+// Verify that dummy grids satisfy the grid_common concept.
+static_assert(simplemc::grid_1d<dummy_grid_1d>);
+static_assert(simplemc::grid_nd<dummy_grid_2d>);
+
+// Test default construction and basic properties.
+TEST(SimplemcGridIterator, DefaultConstruction) {
+    simplemc::grid_iterator<dummy_grid_1d> default_it_1d;
+    ASSERT_EQ(default_it_1d.grid_ptr(), nullptr);
+    ASSERT_EQ(default_it_1d.flat_index(), 0);
+
+    simplemc::grid_iterator<dummy_grid_2d> default_it_2d;
+    ASSERT_EQ(default_it_2d.grid_ptr(), nullptr);
+    ASSERT_EQ(default_it_2d.flat_index(), 0);
+}
+
+// Test construction and dereference for 1D grids.
+TEST(SimplemcGridIterator, Construction1D) {
+    dummy_grid_1d grid(5);
+
+    // construction at index 0
+    simplemc::grid_iterator<dummy_grid_1d> it_begin(grid);
+    ASSERT_EQ(it_begin.grid_ptr(), &grid);
+    ASSERT_EQ(it_begin.flat_index(), 0);
+    ASSERT_EQ(*it_begin, 0.0);
+    ASSERT_EQ(it_begin.nd_index(), 0);
+
+    // construction at specific index
+    simplemc::grid_iterator<dummy_grid_1d> it_mid(grid, 2);
+    ASSERT_EQ(it_mid.flat_index(), 2);
+    ASSERT_EQ(*it_mid, 2.0);
+    ASSERT_EQ(it_mid.nd_index(), 2);
+
+    // construction at end
+    simplemc::grid_iterator<dummy_grid_1d> it_end(grid, 5);
+    ASSERT_EQ(it_end.flat_index(), 5);
+
+    // dereference returns correct values for all indices
+    for (long i = 0; i < grid.size(); ++i) {
+        simplemc::grid_iterator<dummy_grid_1d> curr_it(grid, i);
+        ASSERT_DOUBLE_EQ(*curr_it, grid.at(i));
+        ASSERT_EQ(curr_it.nd_index(), i);
+    }
+}
+
+// Test construction and dereference for 2D grids.
+TEST(SimplemcGridIterator, Construction2D) {
+    dummy_grid_2d grid({ 3, 4 });
+
+    // construction at index 0
+    simplemc::grid_iterator<dummy_grid_2d> it_begin(grid);
+    ASSERT_EQ(it_begin.grid_ptr(), &grid);
+    ASSERT_EQ(it_begin.flat_index(), 0);
+    auto val_begin = *it_begin;
+    ASSERT_DOUBLE_EQ(val_begin[0], 0.0);
+    ASSERT_DOUBLE_EQ(val_begin[1], 0.0);
+
+    auto nd_idx_begin = it_begin.nd_index();
+    ASSERT_EQ(nd_idx_begin[0], 0);
+    ASSERT_EQ(nd_idx_begin[1], 0);
+
+    // construction at specific index
+    simplemc::grid_iterator<dummy_grid_2d> it_mid(grid, 5);
+    ASSERT_EQ(it_mid.flat_index(), 5);
+    auto val_mid = *it_mid;
+    ASSERT_DOUBLE_EQ(val_mid[0], 1.0);
+    ASSERT_DOUBLE_EQ(val_mid[1], 1.0);
+
+    auto nd_idx_mid = it_mid.nd_index();
+    ASSERT_EQ(nd_idx_mid[0], 1);
+    ASSERT_EQ(nd_idx_mid[1], 1);
+
+    // iteration over all flat indices
+    for (long flat_idx = 0; flat_idx < grid.size(); ++flat_idx) {
+        simplemc::grid_iterator<dummy_grid_2d> curr_it(grid, flat_idx);
+        auto idx_2d = simplemc::nd_index(flat_idx, grid.shape(), simplemc::row_major {});
+        auto expected_val = grid.at(idx_2d);
+        auto actual_val = *curr_it;
+
+        ASSERT_DOUBLE_EQ(actual_val[0], expected_val[0]);
+        ASSERT_DOUBLE_EQ(actual_val[1], expected_val[1]);
+
+        auto nd_idx = curr_it.nd_index();
+        ASSERT_EQ(nd_idx[0], idx_2d[0]);
+        ASSERT_EQ(nd_idx[1], idx_2d[1]);
+    }
+}
+
+// Test pre-increment and post-increment operators for 1D grids.
+TEST(SimplemcGridIterator, Increment1D) {
+    dummy_grid_1d grid(5);
+    simplemc::grid_iterator<dummy_grid_1d> it(grid);
+
+    // pre-increment
+    ASSERT_EQ(it.flat_index(), 0);
+    auto& ref = ++it;
+    ASSERT_EQ(it.flat_index(), 1);
+    ASSERT_EQ(*it, 1.0);
+    ASSERT_EQ(&ref, &it);
+
+    // post-increment
+    auto old_it = it++;
+    ASSERT_EQ(it.flat_index(), 2);
+    ASSERT_EQ(*it, 2.0);
+    ASSERT_EQ(old_it.flat_index(), 1);
+    ASSERT_EQ(*old_it, 1.0);
+
+    // multiple increments.
+    ++it;
+    ++it;
+    ASSERT_EQ(it.flat_index(), 4);
+    ASSERT_EQ(*it, 4.0);
+}
+
+// Test pre-increment and post-increment operators for 2D grids.
+TEST(SimplemcGridIterator, Increment2D) {
+    dummy_grid_2d grid({ 3, 4 });
+    simplemc::grid_iterator<dummy_grid_2d> it(grid);
+
+    // pre-increment
+    ASSERT_EQ(it.flat_index(), 0);
+    ++it;
+    ASSERT_EQ(it.flat_index(), 1);
+    auto val1 = *it;
+    ASSERT_DOUBLE_EQ(val1[0], 0.0);
+    ASSERT_DOUBLE_EQ(val1[1], 1.0);
+
+    // post-increment
+    auto old_it = it++;
+    ASSERT_EQ(it.flat_index(), 2);
+    ASSERT_EQ(old_it.flat_index(), 1);
+
+    // increment to next row
+    it = simplemc::grid_iterator<dummy_grid_2d>(grid, 3);
+    ++it;
+    ASSERT_EQ(it.flat_index(), 4);
+    auto val4 = *it;
+    ASSERT_DOUBLE_EQ(val4[0], 1.0);
+    ASSERT_DOUBLE_EQ(val4[1], 0.0);
+}
+
+// Test pre-decrement and post-decrement operators for 1D grids.
+TEST(SimplemcGridIterator, Decrement1D) {
+    dummy_grid_1d grid(5);
+    simplemc::grid_iterator<dummy_grid_1d> it(grid, 4);
+
+    // pre-decrement
+    ASSERT_EQ(it.flat_index(), 4);
+    auto& ref = --it;
+    ASSERT_EQ(it.flat_index(), 3);
+    ASSERT_EQ(*it, 3.0);
+    ASSERT_EQ(&ref, &it);
+
+    // post-decrement
+    auto old_it = it--;
+    ASSERT_EQ(it.flat_index(), 2);
+    ASSERT_EQ(*it, 2.0);
+    ASSERT_EQ(old_it.flat_index(), 3);
+    ASSERT_EQ(*old_it, 3.0);
+
+    // multiple decrements
+    --it;
+    --it;
+    ASSERT_EQ(it.flat_index(), 0);
+    ASSERT_EQ(*it, 0.0);
+}
+
+// Test pre-decrement and post-decrement operators for 2D grids.
+TEST(SimplemcGridIterator, Decrement2D) {
+    dummy_grid_2d grid({ 3, 4 });
+    simplemc::grid_iterator<dummy_grid_2d> it(grid, 11);
+
+    // pre-decrement
+    ASSERT_EQ(it.flat_index(), 11);
+    --it;
+    ASSERT_EQ(it.flat_index(), 10);
+    auto val10 = *it;
+    ASSERT_DOUBLE_EQ(val10[0], 2.0);
+    ASSERT_DOUBLE_EQ(val10[1], 2.0);
+
+    // post-decrement
+    auto old_it = it--;
+    ASSERT_EQ(it.flat_index(), 9);
+    ASSERT_EQ(old_it.flat_index(), 10);
+
+    // decrement across row boundary
+    it = simplemc::grid_iterator<dummy_grid_2d>(grid, 4);
+    --it;
+    ASSERT_EQ(it.flat_index(), 3);
+    auto val3 = *it;
+    ASSERT_DOUBLE_EQ(val3[0], 0.0);
+    ASSERT_DOUBLE_EQ(val3[1], 3.0);
+}
+
+// Test addition assignment operator for 1D grids.
+TEST(SimplemcGridIterator, AdditionAssignment1D) {
+    dummy_grid_1d grid(10);
+    simplemc::grid_iterator<dummy_grid_1d> it(grid);
+
+    it += 3;
+    ASSERT_EQ(it.flat_index(), 3);
+    ASSERT_EQ(*it, 3.0);
+
+    it += 0;
+    ASSERT_EQ(it.flat_index(), 3);
+
+    it += -2;
+    ASSERT_EQ(it.flat_index(), 1);
+    ASSERT_EQ(*it, 1.0);
+}
+
+// Test addition assignment operator for 2D grids.
+TEST(SimplemcGridIterator, AdditionAssignment2D) {
+    dummy_grid_2d grid({ 3, 4 });
+    simplemc::grid_iterator<dummy_grid_2d> it(grid);
+
+    it += 5;
+    ASSERT_EQ(it.flat_index(), 5);
+    auto val5 = *it;
+    ASSERT_DOUBLE_EQ(val5[0], 1.0);
+    ASSERT_DOUBLE_EQ(val5[1], 1.0);
+
+    it += -3;
+    ASSERT_EQ(it.flat_index(), 2);
+    auto val2 = *it;
+    ASSERT_DOUBLE_EQ(val2[0], 0.0);
+    ASSERT_DOUBLE_EQ(val2[1], 2.0);
+}
+
+// Test subtraction assignment operator for 1D grids.
+TEST(SimplemcGridIterator, SubtractionAssignment1D) {
+    dummy_grid_1d grid(10);
+    simplemc::grid_iterator<dummy_grid_1d> it(grid, 7);
+
+    it -= 3;
+    ASSERT_EQ(it.flat_index(), 4);
+    ASSERT_EQ(*it, 4.0);
+
+    it -= 0;
+    ASSERT_EQ(it.flat_index(), 4);
+
+    it -= -2;
+    ASSERT_EQ(it.flat_index(), 6);
+    ASSERT_EQ(*it, 6.0);
+}
+
+// Test subtraction assignment operator for 2D grids.
+TEST(SimplemcGridIterator, SubtractionAssignment2D) {
+    dummy_grid_2d grid({ 3, 4 });
+    simplemc::grid_iterator<dummy_grid_2d> it(grid, 10);
+
+    it -= 5;
+    ASSERT_EQ(it.flat_index(), 5);
+    auto val5 = *it;
+    ASSERT_DOUBLE_EQ(val5[0], 1.0);
+    ASSERT_DOUBLE_EQ(val5[1], 1.0);
+
+    it -= -3;
+    ASSERT_EQ(it.flat_index(), 8);
+    auto val8 = *it;
+    ASSERT_DOUBLE_EQ(val8[0], 2.0);
+    ASSERT_DOUBLE_EQ(val8[1], 0.0);
+}
+
+// Test iterator addition operator for 1D grids.
+TEST(SimplemcGridIterator, Addition1D) {
+    dummy_grid_1d grid(10);
+    simplemc::grid_iterator<dummy_grid_1d> it(grid, 2);
+
+    // it + n
+    auto it_plus_3 = it + 3;
+    ASSERT_EQ(it.flat_index(), 2);
+    ASSERT_EQ(it_plus_3.flat_index(), 5);
+    ASSERT_EQ(*it_plus_3, 5.0);
+
+    // n + it
+    auto three_plus_it = 4 + it;
+    ASSERT_EQ(three_plus_it.flat_index(), 6);
+    ASSERT_EQ(*three_plus_it, 6.0);
+
+    // negative offset
+    auto it_minus = it + (-1);
+    ASSERT_EQ(it_minus.flat_index(), 1);
+    ASSERT_EQ(*it_minus, 1.0);
+}
+
+// Test iterator addition operator for 2D grids.
+TEST(SimplemcGridIterator, Addition2D) {
+    dummy_grid_2d grid({ 3, 4 });
+    simplemc::grid_iterator<dummy_grid_2d> it(grid, 2);
+
+    auto it_plus_5 = it + 5;
+    ASSERT_EQ(it.flat_index(), 2);
+    ASSERT_EQ(it_plus_5.flat_index(), 7);
+    auto val7 = *it_plus_5;
+    ASSERT_DOUBLE_EQ(val7[0], 1.0);
+    ASSERT_DOUBLE_EQ(val7[1], 3.0);
+
+    auto five_plus_it = 3 + it;
+    ASSERT_EQ(five_plus_it.flat_index(), 5);
+}
+
+// Test iterator subtraction operator for 1D grids.
+TEST(SimplemcGridIterator, Subtraction1D) {
+    dummy_grid_1d grid(10);
+    simplemc::grid_iterator<dummy_grid_1d> it(grid, 7);
+
+    // it - n
+    auto it_minus_3 = it - 3;
+    ASSERT_EQ(it.flat_index(), 7);
+    ASSERT_EQ(it_minus_3.flat_index(), 4);
+    ASSERT_EQ(*it_minus_3, 4.0);
+
+    // it1 - it2
+    simplemc::grid_iterator<dummy_grid_1d> it2(grid, 2);
+    auto diff = it - it2;
+    ASSERT_EQ(diff, 5);
+
+    auto diff_reverse = it2 - it;
+    ASSERT_EQ(diff_reverse, -5);
+
+    // same iterator
+    auto diff_same = it - it;
+    ASSERT_EQ(diff_same, 0);
+}
+
+// Test iterator subtraction operator for 2D grids.
+TEST(SimplemcGridIterator, Subtraction2D) {
+    dummy_grid_2d grid({ 3, 4 });
+    simplemc::grid_iterator<dummy_grid_2d> it(grid, 10);
+
+    auto it_minus_5 = it - 5;
+    ASSERT_EQ(it.flat_index(), 10);
+    ASSERT_EQ(it_minus_5.flat_index(), 5);
+    auto val5 = *it_minus_5;
+    ASSERT_DOUBLE_EQ(val5[0], 1.0);
+    ASSERT_DOUBLE_EQ(val5[1], 1.0);
+
+    // iterator difference
+    simplemc::grid_iterator<dummy_grid_2d> it2(grid, 3);
+    auto diff = it - it2;
+    ASSERT_EQ(diff, 7);
+}
+
+// Test subscript operator for 1D grids.
+TEST(SimplemcGridIterator, Subscript1D) {
+    dummy_grid_1d grid(10);
+    simplemc::grid_iterator<dummy_grid_1d> it(grid, 3);
+
+    // positive offsets
+    ASSERT_EQ(it[0], 3.0);
+    ASSERT_EQ(it[1], 4.0);
+    ASSERT_EQ(it[2], 5.0);
+
+    // negative offsets
+    ASSERT_EQ(it[-1], 2.0);
+    ASSERT_EQ(it[-2], 1.0);
+    ASSERT_EQ(it[-3], 0.0);
+
+    // verify original iterator unchanged
+    ASSERT_EQ(it.flat_index(), 3);
+}
+
+// Test subscript operator for 2D grids.
+TEST(SimplemcGridIterator, Subscript2D) {
+    dummy_grid_2d grid({ 3, 4 });
+    simplemc::grid_iterator<dummy_grid_2d> it(grid, 5);
+
+    // positive offsets
+    auto val0 = it[0];
+    ASSERT_DOUBLE_EQ(val0[0], 1.0);
+    ASSERT_DOUBLE_EQ(val0[1], 1.0);
+
+    auto val1 = it[1];
+    ASSERT_DOUBLE_EQ(val1[0], 1.0);
+    ASSERT_DOUBLE_EQ(val1[1], 2.0);
+
+    auto val3 = it[3];
+    ASSERT_DOUBLE_EQ(val3[0], 2.0);
+    ASSERT_DOUBLE_EQ(val3[1], 0.0);
+
+    // negative offsets
+    auto val_neg1 = it[-1];
+    ASSERT_DOUBLE_EQ(val_neg1[0], 1.0);
+    ASSERT_DOUBLE_EQ(val_neg1[1], 0.0);
+
+    // verify original iterator unchanged.
+    ASSERT_EQ(it.flat_index(), 5);
+}
+
+// Test equality comparison operator.
+TEST(SimplemcGridIterator, EqualityComparison) {
+    dummy_grid_1d grid1(5);
+    dummy_grid_1d grid2(5);
+
+    simplemc::grid_iterator<dummy_grid_1d> it1(grid1, 2);
+    simplemc::grid_iterator<dummy_grid_1d> it2(grid1, 2);
+    simplemc::grid_iterator<dummy_grid_1d> it3(grid1, 3);
+    simplemc::grid_iterator<dummy_grid_1d> it4(grid2, 2);
+
+    // equality
+    ASSERT_TRUE(it1 == it2);
+    ASSERT_FALSE(it1 != it2);
+
+    // inequality (different index)
+    ASSERT_FALSE(it1 == it3);
+    ASSERT_TRUE(it1 != it3);
+
+    // inequality (different grid)
+    ASSERT_FALSE(it1 == it4);
+    ASSERT_TRUE(it1 != it4);
+}
+
+// Test three-way comparison operator for 1D grids.
+TEST(SimplemcGridIterator, ThreeWayComparison1D) {
+    dummy_grid_1d grid(10);
+
+    simplemc::grid_iterator<dummy_grid_1d> it1(grid, 2);
+    simplemc::grid_iterator<dummy_grid_1d> it2(grid, 5);
+    simplemc::grid_iterator<dummy_grid_1d> it3(grid, 2);
+
+    // less than
+    ASSERT_TRUE(it1 < it2);
+    ASSERT_FALSE(it2 < it1);
+    ASSERT_FALSE(it1 < it3);
+
+    // less than or equal
+    ASSERT_TRUE(it1 <= it2);
+    ASSERT_TRUE(it1 <= it3);
+    ASSERT_FALSE(it2 <= it1);
+
+    // greater than
+    ASSERT_TRUE(it2 > it1);
+    ASSERT_FALSE(it1 > it2);
+    ASSERT_FALSE(it1 > it3);
+
+    // greater than or equal
+    ASSERT_TRUE(it2 >= it1);
+    ASSERT_TRUE(it1 >= it3);
+    ASSERT_FALSE(it1 >= it2);
+}
+
+// Test three-way comparison operator for 2D grids.
+TEST(SimplemcGridIterator, ThreeWayComparison2D) {
+    dummy_grid_2d grid({ 3, 4 });
+
+    simplemc::grid_iterator<dummy_grid_2d> it1(grid, 3);
+    simplemc::grid_iterator<dummy_grid_2d> it2(grid, 8);
+    simplemc::grid_iterator<dummy_grid_2d> it3(grid, 3);
+
+    ASSERT_TRUE(it1 < it2);
+    ASSERT_TRUE(it1 <= it2);
+    ASSERT_TRUE(it1 <= it3);
+    ASSERT_TRUE(it2 > it1);
+    ASSERT_TRUE(it2 >= it1);
+    ASSERT_TRUE(it1 >= it3);
+}
+
+// Test dimension queries.
+TEST(SimplemcGridIterator, DimensionQueries) {
+    dummy_grid_1d grid_1d(5);
+    dummy_grid_2d grid_2d({ 3, 4 });
+
+    simplemc::grid_iterator<dummy_grid_1d> it_1d(grid_1d);
+    simplemc::grid_iterator<dummy_grid_2d> it_2d(grid_2d);
+
+    ASSERT_EQ(it_1d.dim(), 1);
+    ASSERT_EQ(it_2d.dim(), 2);
+}
