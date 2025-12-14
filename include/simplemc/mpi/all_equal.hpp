f8e2a5d338c666af7262316523f7b6374d586ccb
Thomas Hahn <thomas.hahn01@gmail.com>
2025-12-14 06:48:11 -0500

    Move all_equal function to its own header
    - provide overload for mpi_range types
    - call MPI functions directly --> no circular dependencies

---
+++ include/simplemc/mpi/all_equal.hpp
@@ -0,0 +1,101 @@
+/**
+ * @file
+ * @brief Check that a value or range of values is equal across all MPI processes.
+ */
+
+#ifndef SIMPLEMC_MPI_ALL_EQUAL_HPP
+#define SIMPLEMC_MPI_ALL_EQUAL_HPP
+
+#include <simplemc/mpi/mpi_type.hpp>
+#include <simplemc/mpi/utils.hpp>
+#include <simplemc/utils/ranges.hpp>
+
+#include <mpi.h>
+
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+namespace simplemc::mpi {
+
+/**
+ * @addtogroup simplemc-mpi-coll
+ * @{
+ */
+
+/**
+ * @brief Check if a value is equal across all processes.
+ *
+ * @details It performs the following steps:
+ *
+ * - `MPI_Bcast` the value from rank 0 to all other processes.
+ * - Each process equality compares its local value to the broadcasted value.
+ * - `MPI_Allreduce` the comparison results using `MPI_LAND`.
+ *
+ * @note This function involves two collective operations, so all processes must call it.
+ *
+ * @tparam T simplemc::mpi::mpi_compatible type.
+ * @param in_value Value to compare across processes.
+ * @param comm MPI communicator.
+ * @return True if all processes have the same value, false otherwise.
+ */
+template <mpi_compatible T>
+[[nodiscard]] bool all_equal(const T& in_value, MPI_Comm comm) {
+    // broadcast the value from rank 0
+    T root_value = in_value;
+    check_mpi_call(MPI_Bcast(&root_value, 1, mpi_type<T>::get(), 0, comm), "MPI_Bcast");
+
+    // compare local value with broadcasted value
+    const int local_equal = (in_value == root_value);
+
+    // broadcast the comparison result
+    int all_equal {};
+    check_mpi_call(MPI_Allreduce(&local_equal, &all_equal, 1, mpi_type<int>::get(), MPI_LAND, comm), "MPI_Allreduce");
+    return all_equal;
+}
+
+/**
+ * @brief Check if all elements in a range are equal across all processes.
+ *
+ * @details It performs the following steps:
+ *
+ * - Check that the ranges sizes are simplemc::mpi::all_equal.
+ * - `MPI_Bcast` the range from rank 0 to all other processes.
+ * - Each process equality compares its local range element-wise to the broadcasted range.
+ * - `MPI_Allreduce` the comparison results using `MPI_LAND`.
+ *
+ * @note This function involves three collective operations, so all processes must call it.
+ *
+ * @tparam R simplemc::mpi::mpi_range type.
+ * @param rg Range to compare across processes.
+ * @param comm MPI communicator.
+ * @return True if all processes have ranges with identical size and content, false otherwise.
+ */
+template <mpi_range R>
+[[nodiscard]] bool all_equal(R&& rg, MPI_Comm comm) { // NOLINT (ranges need not be forwarded)
+    using value_type = ranges::range_value_t<R>;
+
+    // check if all processes have the same size
+    if (!all_equal(ranges::size(rg), comm)) {
+        return false;
+    }
+
+    // broadcast the range from rank 0
+    auto root_rg = std::vector(ranges::begin(rg), ranges::end(rg));
+    check_mpi_call(
+        MPI_Bcast(root_rg.data(), static_cast<int>(root_rg.size()), mpi_type<value_type>::get(), 0, comm), "MPI_Bcast");
+
+    // compare local range with broadcasted values
+    const int local_equal = std::ranges::equal(rg, root_rg) ? 1 : 0;
+
+    // broadcast the comparison result
+    int all_equal {};
+    check_mpi_call(MPI_Allreduce(&local_equal, &all_equal, 1, mpi_type<int>::get(), MPI_LAND, comm), "MPI_Allreduce");
+    return all_equal;
+}
+
+/** @} */
+
+} // namespace simplemc::mpi
+
+#endif // SIMPLEMC_MPI_ALL_EQUAL_HPP
---
+++ tests/mpi/mpi_allequal_test.cpp
@@ -0,0 +1,57 @@
+#include "../gtest-mpi-listener.hpp"
+
+#include <gtest/gtest.h>
+#include <simplemc/mpi.hpp>
+
+#include <complex>
+#include <vector>
+
+// Test all_equal for scalars.
+TEST(SimplemcMPI, AllEqualScalars) {
+    simplemc::mpi::communicator comm {};
+    ASSERT_TRUE(simplemc::mpi::all_equal(1000, comm));
+    ASSERT_EQ(simplemc::mpi::all_equal(comm.rank(), comm), comm.size() == 1);
+    ASSERT_TRUE(simplemc::mpi::all_equal(3.14159, comm));
+    ASSERT_EQ(simplemc::mpi::all_equal(3.14159 + comm.rank(), comm), comm.size() == 1);
+    ASSERT_TRUE(simplemc::mpi::all_equal(std::complex<double>(1.0, 2.0), comm));
+    ASSERT_EQ(simplemc::mpi::all_equal(std::complex<double>(comm.rank(), 2.0), comm), comm.size() == 1);
+}
+
+// Test all_equal for ranges.
+TEST(SimplemcMPI, AllEqualRanges) {
+    simplemc::mpi::communicator comm {};
+    ASSERT_TRUE(simplemc::mpi::all_equal(std::vector<char>(), comm));
+    auto data = std::vector<double> { 1.0, 2.0, 3.0, 4.0 };
+    ASSERT_TRUE(simplemc::mpi::all_equal(data, comm));
+    data[0] = comm.rank();
+    ASSERT_EQ(simplemc::mpi::all_equal(data, comm), comm.size() == 1);
+    auto data_2 = std::vector<int>(comm.rank() + 1, 42);
+    ASSERT_EQ(simplemc::mpi::all_equal(data_2, comm), comm.size() == 1);
+}
+
+// Custom main function for MPI.
+int main(int argc, char** argv) {
+    // filter out Google Test arguments
+    ::testing::InitGoogleTest(&argc, argv);
+
+    // initialize MPI
+    MPI_Init(&argc, &argv);
+
+    // add object that will finalize MPI on exit; Google Test owns this pointer
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+
+    // get the event listener list.
+    ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
+
+    // remove default listener: the default printer and the default XML printer
+    ::testing::TestEventListener* l = listeners.Release(listeners.default_result_printer());
+
+    // adds MPI listener; Google Test owns this pointer
+    listeners.Append(new GTestMPIListener::MPIWrapperPrinter(l, MPI_COMM_WORLD));
+
+    // run tests, then clean up and exit. RUN_ALL_TESTS() returns 0 if all tests
+    // pass and 1 if some test fails.
+    int result = RUN_ALL_TESTS();
+
+    return result;
+}
