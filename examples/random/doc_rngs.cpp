bdd91e1fa377b53c51e95f1bcb5987689f098313
Thomas Hahn <thomas.hahn01@gmail.com>
2026-01-01 06:23:23 -0500

    Move doc example of simplemc-random-rngs to examples folder

---
+++ examples/random/doc_rngs.cpp
@@ -0,0 +1,15 @@
+#include <fmt/base.h>
+#include <simplemc/random.hpp>
+
+#include <random>
+
+int main() {
+    // construct a xoshiro RNG
+    simplemc::xoshiro256pp rng {};
+
+    // print 5 random samples generated from a normal distribution
+    std::normal_distribution<> dist { 0.0, 1.0 };
+    for (int i = 0; i < 5; ++i) {
+        fmt::println("{}", dist(rng));
+    }
+}
