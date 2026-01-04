b48f5985e1f958ef22d86005688c1eb54dcfde61
Thomas Hahn <thomas.hahn01@gmail.com>
2026-01-04 10:35:45 -0500

    Update simplemc-numeric-lattices docs
    - move example code to examples folder

---
+++ examples/numeric/doc_lattices.cpp
@@ -0,0 +1,20 @@
+#include <fmt/base.h>
+#include <simplemc/numeric.hpp>
+#include <simplemc/utils.hpp>
+
+int main() {
+    // create an FCC lattice with a lattic constant of 1.5
+    auto [fcc, lattice_params] = simplemc::make_fcc_lattice(1.5);
+
+    // print the lattice vectors of the primitive cell
+    fmt::println("Lattice vectors:\n{}\n", simplemc::to_string(fcc.real_lattice_vectors()));
+
+    // print the volume of the primitive cell
+    fmt::println("Volume: {}\n", fcc.real_cell_volume());
+
+    // print the reciprocal lattice vectors
+    fmt::println("Reciprocal lattice vectors:\n{}\n", simplemc::to_string(fcc.reciprocal_lattice_vectors()));
+
+    // print the volume of the reciprocal cell
+    fmt::println("Reciprocal volume: {}", fcc.reciprocal_cell_volume());
+}
