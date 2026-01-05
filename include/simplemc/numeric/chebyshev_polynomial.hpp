9f455477535765be7eb262d3518bfa29c778fcb0
Thomas Hahn <thomas.hahn01@gmail.com>
2026-01-05 08:19:23 -0500

    Update chebyshev polynomials
    - add [[nodiscard]], constexpr, noexcept
    - make everything constexpr, remove cpp file

--- include/simplemc/numeric/chebyshev_polynomial.hpp
+++ include/simplemc/numeric/chebyshev_polynomial.hpp
@@ -7,6 +7,8 @@
 #ifndef SIMPLEMC_NUMERIC_CHEBYSHEV_POLYNOMIAL_HPP
 #define SIMPLEMC_NUMERIC_CHEBYSHEV_POLYNOMIAL_HPP
 
+#include <simplemc/utils/simplemc_exception.hpp>
+
 #include <array>
 #include <cassert>
 #include <cmath>
@@ -58,7 +60,7 @@ public:
      *
      * @return `std::array<double, 2>` representing the interval \f$ \mathrm{D} = [-1, 1] \f$.
      */
-    [[nodiscard]] static constexpr auto domain() { return std::array<double, 2> { -1.0, 1.0 }; }
+    [[nodiscard]] static constexpr auto domain() noexcept { return std::array<double, 2> { -1.0, 1.0 }; }
 
     /**
      * @brief Get the normalization factor \f$ N_l = \langle T_l, T_l \rangle \f$ of the l<sup>th
@@ -77,7 +79,12 @@ public:
      * @param l Order of the polynomial.
      * @return Normalization factor \f$ N_l \f$.
      */
-    [[nodiscard]] static double norm(int l);
+    [[nodiscard]] static constexpr double norm(int l) noexcept {
+        if (l == 0) {
+            return std::numbers::pi;
+        }
+        return 0.5 * std::numbers::pi;
+    }
 
     /**
      * @brief Get the value of the weight function \f$ W(x) \f$ evaluated at a given \f$ x \f$.
@@ -85,10 +92,7 @@ public:
      * @param x Value at which the weight function is evaluated.
      * @return Weight function \f$ W(x) = 1 / \sqrt{1 - x^2} \f$.
      */
-    [[nodiscard]] static double weight(double x) {
-        assert(std::abs(x) <= 1.0);
-        return 1.0 / std::sqrt(1 - x * x);
-    }
+    [[nodiscard]] static constexpr double weight(double x) noexcept { return 1.0 / std::sqrt(1 - x * x); }
 
     /**
      * @brief Construct a sequence of Chebyshev polynomials of the first kind for a specific \f$ x \f$
@@ -97,57 +101,61 @@ public:
      * @details It sets \f$ x \f$ and initializes \f$ l = 0 \f$, \f$ T_0(x) = 1 \f$, \f$ T_1(x) = x
      * \f$, \f$ T'_0(x) = 0 \f$ and \f$ T'_1(x) = 1 \f$.
      *
+     * It throws a simplemc::simplemc_exception if \f$ |x| > 1 \f$.
+     *
      * @param x Value at which the polynomials and its derivatives are evaluated.
      */
-    chebyshev_polynomial_first(double x = 0.0);
+    constexpr chebyshev_polynomial_first(double x = 0.0) : x_(x) {
+        if (std::abs(x_) > 1) {
+            throw simplemc_exception("Chebyshev polynomials are only defined on the interval [-1, 1]");
+        }
+    }
 
     /**
      * @brief Get the order \f$ l \f$ of the currently processed polynomial \f$ T_l(x) \f$.
      *
      * @return Order \f$ l \f$.
      */
-    [[nodiscard]] int order() const { return l_; }
+    [[nodiscard]] constexpr int order() const noexcept { return l_; }
 
     /**
      * @brief Get the \f$ x \f$ value at which the polynomials are evaluated.
      *
      * @return \f$ x \f$ value given at construction.
      */
-    [[nodiscard]] double x() const { return x_; }
+    [[nodiscard]] constexpr double x() const noexcept { return x_; }
 
     /**
      * @brief Get the function value of the currently processed polynomial.
      *
      * @return \f$ T_l(x) \f$.
      */
-    [[nodiscard]] double current() const { return tl_; }
+    [[nodiscard]] constexpr double current() const noexcept { return tl_; }
 
     /**
      * @brief Get the function value of the previously processed polynomial.
      *
+     * @note The behaviour is undefined for \f$ l = 0 \f$.
+     *
      * @return \f$ T_{l-1}(x) \f$.
      */
-    [[nodiscard]] double prev() const {
-        assert(l_ > 0);
-        return tlm1_;
-    }
+    [[nodiscard]] constexpr double prev() const noexcept { return tlm1_; }
 
     /**
      * @brief Get the derivative of the currently processed polynomial.
      *
      * @return \f$ T'_l(x) \f$.
      */
-    [[nodiscard]] double current_derivative() const { return dtl_; };
+    [[nodiscard]] constexpr double current_derivative() const noexcept { return dtl_; }
 
     /**
      * @brief Get the derivative of the previously processed polynomial.
      *
+     * @note The behaviour is undefined for \f$ l = 0 \f$.
+     *
      * @return \f$ T'_{l-1}(x) \f$.
      */
-    [[nodiscard]] double prev_derivative() const {
-        assert(l_ > 0);
-        return dtlm1_;
-    };
+    [[nodiscard]] constexpr double prev_derivative() const noexcept { return dtlm1_; }
 
     /**
      * @brief Increase the order from \f$ l \f$ to \f$ l + 1 \f$.
@@ -163,15 +171,28 @@ public:
      *
      * @return Function value of the polynomial \f$ T_l(x) \f$.
      */
-    double next();
+    constexpr double next() noexcept {
+        double tlp1 = x_;
+        double dtlp1 = 1;
+        if (l_ > 0) {
+            tlp1 = 2.0 * x_ * tl_ - tlm1_;
+            dtlp1 = 2.0 * (tl_ + x_ * dtl_) - dtlm1_;
+        }
+        tlm1_ = tl_;
+        tl_ = tlp1;
+        dtlm1_ = dtl_;
+        dtl_ = dtlp1;
+        ++l_;
+        return tlm1_;
+    }
 
 private:
     int l_ { 0 };
     double x_;
-    double tlm1_;
-    double tl_;
-    double dtlm1_;
-    double dtl_;
+    double tlm1_ { 0.0 };
+    double tl_ { 1.0 };
+    double dtlm1_ { 0.0 };
+    double dtl_ { 0.0 };
 };
 
 /**
@@ -208,7 +229,7 @@ public:
      *
      * @return `std::array<double, 2>` representing the interval \f$ \mathrm{D} = [-1, 1] \f$.
      */
-    [[nodiscard]] static constexpr auto domain() { return std::array<double, 2> { -1.0, 1.0 }; }
+    [[nodiscard]] static constexpr auto domain() noexcept { return std::array<double, 2> { -1.0, 1.0 }; }
 
     /**
      * @brief Get the normalization factor \f$ N_l = \langle U_l, U_l \rangle \f$ of the l<sup>th
@@ -217,7 +238,7 @@ public:
      * @param l Order of the polynomial.
      * @return Normalization factor \f$ N_l = \pi / 2 \f$.
      */
-    [[nodiscard]] static constexpr double norm([[maybe_unused]] int l) { return 0.5 * std::numbers::pi; }
+    [[nodiscard]] static constexpr double norm([[maybe_unused]] int l) noexcept { return 0.5 * std::numbers::pi; }
 
     /**
      * @brief Get the value of the weight function \f$ W(x) \f$ evaluated at a given \f$ x \f$.
@@ -225,10 +246,7 @@ public:
      * @param x Value at which the weight function is evaluated.
      * @return Weight function \f$ W(x) = \sqrt{1 - x^2} \f$.
      */
-    [[nodiscard]] static double weight(double x) {
-        assert(std::abs(x) <= 1.0);
-        return std::sqrt(1 - x * x);
-    }
+    [[nodiscard]] static constexpr double weight(double x) noexcept { return std::sqrt(1 - x * x); }
 
     /**
      * @brief Construct a sequence of Chebyshev polynomials of the second kind for a specific \f$ x
@@ -237,57 +255,61 @@ public:
      * @details It sets \f$ x \f$ and initializes \f$ l = 0 \f$, \f$ U_0(x) = 1 \f$, \f$ U_1(x) = 2 x
      * \f$, \f$ U'_0(x) = 0 \f$ and \f$ U'_1(x) = 2 \f$.
      *
+     * It throws a simplemc::simplemc_exception if \f$ |x| > 1 \f$.
+     *
      * @param x Value at which the polynomials and its derivatives are evaluated.
      */
-    chebyshev_polynomial_second(double x = 0.0);
+    constexpr chebyshev_polynomial_second(double x = 0.0) : x_(x) {
+        if (std::abs(x_) > 1) {
+            throw simplemc_exception("Chebyshev polynomials are only defined on the interval [-1, 1]");
+        }
+    }
 
     /**
      * @brief Get the order \f$ l \f$ of the currently processed polynomial \f$ U_l(x) \f$.
      *
      * @return Order \f$ l \f$.
      */
-    [[nodiscard]] int order() const { return l_; }
+    [[nodiscard]] constexpr int order() const noexcept { return l_; }
 
     /**
      * @brief Get the \f$ x \f$ value at which the polynomials are evaluated.
      *
      * @return \f$ x \f$ value given at construction.
      */
-    [[nodiscard]] double x() const { return x_; }
+    [[nodiscard]] constexpr double x() const noexcept { return x_; }
 
     /**
      * @brief Get the function value of the currently processed polynomial.
      *
      * @return \f$ U_l(x) \f$.
      */
-    [[nodiscard]] double current() const { return ul_; }
+    [[nodiscard]] constexpr double current() const noexcept { return ul_; }
 
     /**
      * @brief Get the function value of the previously processed polynomial.
      *
+     * @note The behaviour is undefined for \f$ l = 0 \f$.
+     *
      * @return \f$ U_{l-1}(x) \f$.
      */
-    [[nodiscard]] double prev() const {
-        assert(l_ > 0);
-        return ulm1_;
-    }
+    [[nodiscard]] constexpr double prev() const noexcept { return ulm1_; }
 
     /**
      * @brief Get the derivative of the currently processed polynomial.
      *
      * @return \f$ U'_l(x) \f$.
      */
-    [[nodiscard]] double current_derivative() const { return dul_; };
+    [[nodiscard]] constexpr double current_derivative() const noexcept { return dul_; }
 
     /**
      * @brief Get the derivative of the previously processed polynomial.
      *
+     * @note The behaviour is undefined for \f$ l = 0 \f$.
+     *
      * @return \f$ U'_{l-1}(x) \f$.
      */
-    [[nodiscard]] double prev_derivative() const {
-        assert(l_ > 0);
-        return dulm1_;
-    };
+    [[nodiscard]] constexpr double prev_derivative() const noexcept { return dulm1_; }
 
     /**
      * @brief Increase the order from \f$ l \f$ to \f$ l + 1 \f$.
@@ -303,15 +325,28 @@ public:
      *
      * @return Function value of the polynomial \f$ U_l(x) \f$.
      */
-    double next();
+    constexpr double next() noexcept {
+        double ulp1 = 2.0 * x_;
+        double dulp1 = 2.0;
+        if (l_ > 0) {
+            ulp1 = 2.0 * x_ * ul_ - ulm1_;
+            dulp1 = 2.0 * (ul_ + x_ * dul_) - dulm1_;
+        }
+        ulm1_ = ul_;
+        ul_ = ulp1;
+        dulm1_ = dul_;
+        dul_ = dulp1;
+        ++l_;
+        return ulm1_;
+    }
 
 private:
     int l_ { 0 };
     double x_;
-    double ulm1_;
-    double ul_;
-    double dulm1_;
-    double dul_;
+    double ulm1_ { 0.0 };
+    double ul_ { 1.0 };
+    double dulm1_ { 0.0 };
+    double dul_ { 0.0 };
 };
 
 /** @} */
