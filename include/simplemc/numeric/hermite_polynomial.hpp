9252c4afd8bf312a33886b152982d32f582c80b8
Thomas Hahn <thomas.hahn01@gmail.com>
2026-01-05 08:20:20 -0500

    Update hermite_polynomial
    - add [[nodiscard]], constexpr, noexcept
    - make everything constexpr, remove cpp file

--- include/simplemc/numeric/hermite_polynomial.hpp
+++ include/simplemc/numeric/hermite_polynomial.hpp
@@ -54,7 +54,7 @@ public:
      * @return `std::array<double, 2>` representing the interval \f$ \mathrm{D} = (-\infty, \infty)
      * \f$.
      */
-    [[nodiscard]] static constexpr auto domain() {
+    [[nodiscard]] static constexpr auto domain() noexcept {
         return std::array<double, 2> { -std::numeric_limits<double>::infinity(),
             std::numeric_limits<double>::infinity() };
     }
@@ -66,8 +66,7 @@ public:
      * @param l Order of the polynomial.
      * @return Normalization factor \f$ N_l = \sqrt{\pi} 2^l l! \f$.
      */
-    [[nodiscard]] static double norm(int l) {
-        assert(l >= 0);
+    [[nodiscard]] static constexpr double norm(int l) noexcept {
         return std::pow(2, l) * std::tgamma(l + 1) / std::numbers::inv_sqrtpi;
     }
 
@@ -77,7 +76,7 @@ public:
      * @param x Value at which the weight function is evaluated.
      * @return Weight function \f$ W(x) = e^{-x^2} \f$.
      */
-    [[nodiscard]] static double weight([[maybe_unused]] double x) { return std::exp(-x * x); }
+    [[nodiscard]] static constexpr double weight(double x) noexcept { return std::exp(-x * x); }
 
     /**
      * @brief Construct a sequence of Hermite polynomials for a specific \f$ x \f$ value.
@@ -87,55 +86,53 @@ public:
      *
      * @param x Value at which the polynomials and its derivatives are evaluated.
      */
-    hermite_polynomial(double x = 0.0);
+    constexpr hermite_polynomial(double x = 0.0) noexcept : x_(x) {}
 
     /**
      * @brief Get the order \f$ l \f$ of the currently processed polynomial \f$ H_l(x) \f$.
      *
      * @return Order \f$ l \f$.
      */
-    [[nodiscard]] int order() const { return l_; }
+    [[nodiscard]] constexpr int order() const noexcept { return l_; }
 
     /**
      * @brief Get the \f$ x \f$ value at which the polynomials are evaluated.
      *
      * @return \f$ x \f$ value given at construction.
      */
-    [[nodiscard]] double x() const { return x_; }
+    [[nodiscard]] constexpr double x() const noexcept { return x_; }
 
     /**
      * @brief Get the function value of the currently processed polynomial.
      *
      * @return \f$ H_l(x) \f$.
      */
-    [[nodiscard]] double current() const { return hl_; }
+    [[nodiscard]] constexpr double current() const noexcept { return hl_; }
 
     /**
      * @brief Get the function value of the previously processed polynomial.
      *
+     * @note The behaviour is undefined for \f$ l = 0 \f$.
+     *
      * @return \f$ H_{l-1}(x) \f$.
      */
-    [[nodiscard]] double prev() const {
-        assert(l_ > 0);
-        return hlm1_;
-    }
+    [[nodiscard]] constexpr double prev() const noexcept { return hlm1_; }
 
     /**
      * @brief Get the derivative of the currently processed polynomial.
      *
      * @return \f$ H'_l(x) \f$.
      */
-    [[nodiscard]] double current_derivative() const { return dhl_; };
+    [[nodiscard]] constexpr double current_derivative() const noexcept { return dhl_; }
 
     /**
      * @brief Get the derivative of the previously processed polynomial.
      *
+     * @note The behaviour is undefined for \f$ l = 0 \f$.
+     *
      * @return \f$ H'_{l-1}(x) \f$.
      */
-    [[nodiscard]] double prev_derivative() const {
-        assert(l_ > 0);
-        return dhlm1_;
-    };
+    [[nodiscard]] constexpr double prev_derivative() const noexcept { return dhlm1_; }
 
     /**
      * @brief Increase the order from \f$ l \f$ to \f$ l + 1 \f$.
@@ -151,15 +148,28 @@ public:
      *
      * @return Function value of the polynomial \f$ H_l(x) \f$.
      */
-    double next();
+    constexpr double next() noexcept {
+        double hlp1 = 2.0 * x_;
+        double dhlp1 = 2.0;
+        if (l_ > 0) {
+            hlp1 = 2.0 * x_ * hl_ - 2.0 * l_ * hlm1_;
+            dhlp1 = 2.0 * (l_ + 1) * hl_;
+        }
+        hlm1_ = hl_;
+        hl_ = hlp1;
+        dhlm1_ = dhl_;
+        dhl_ = dhlp1;
+        ++l_;
+        return hlm1_;
+    }
 
 private:
     int l_ { 0 };
     double x_;
-    double hlm1_;
-    double hl_;
-    double dhlm1_;
-    double dhl_;
+    double hlm1_ { 0.0 };
+    double hl_ { 1.0 };
+    double dhlm1_ { 0.0 };
+    double dhl_ { 0.0 };
 };
 
 /** @} */
