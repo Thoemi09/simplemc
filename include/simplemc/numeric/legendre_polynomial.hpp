f194efa926c55276507b4f25d3f562102b9e5477
Thomas Hahn <thomas.hahn01@gmail.com>
2026-01-05 07:53:19 -0500

    Update legendre_polynomial
    - add [[nodiscard]], constexpr, noexcept
    - make everything constexpr, remove cpp file

--- include/simplemc/numeric/legendre_polynomial.hpp
+++ include/simplemc/numeric/legendre_polynomial.hpp
@@ -6,8 +6,11 @@
 #ifndef SIMPLEMC_NUMERIC_LEGENDRE_POLYNOMIAL_HPP
 #define SIMPLEMC_NUMERIC_LEGENDRE_POLYNOMIAL_HPP
 
+#include <simplemc/utils/simplemc_exception.hpp>
+
 #include <array>
 #include <cassert>
+#include <cmath>
 
 namespace simplemc {
 
@@ -50,7 +53,7 @@ public:
      *
      * @return `std::array<double, 2>` representing the interval \f$ \mathrm{D} = [-1, 1] \f$.
      */
-    [[nodiscard]] static constexpr auto domain() { return std::array<double, 2> { -1.0, 1.0 }; }
+    [[nodiscard]] static constexpr auto domain() noexcept { return std::array<double, 2> { -1.0, 1.0 }; }
 
     /**
      * @brief Get the normalization factor \f$ N_l = \langle P_l, P_l \rangle \f$ of the
@@ -59,10 +62,7 @@ public:
      * @param l Order of the polynomial.
      * @return Normalization factor \f$ N_l = 2 / (2 l + 1) \f$.
      */
-    [[nodiscard]] static double norm(int l) {
-        assert(l >= 0);
-        return 2.0 / (2.0 * l + 1);
-    }
+    [[nodiscard]] static constexpr double norm(int l) noexcept { return 2.0 / (2.0 * l + 1); }
 
     /**
      * @brief Get the value of the weight function \f$ W(x) \f$ evaluated at a given \f$ x \f$.
@@ -70,7 +70,7 @@ public:
      * @param x Value at which the weight function is evaluated.
      * @return Weight function \f$ W(x) = 1 \f$.
      */
-    [[nodiscard]] static double weight([[maybe_unused]] double x) { return 1.0; }
+    [[nodiscard]] static constexpr double weight([[maybe_unused]] double x) noexcept { return 1.0; }
 
     /**
      * @brief Construct a sequence of Legendre polynomials for a specific \f$ x \f$ value.
@@ -78,57 +78,61 @@ public:
      * @details It sets \f$ x \f$ and initializes \f$ l = 0 \f$, \f$ P_0(x) = 1 \f$, \f$ P_1(x) = x
      * \f$, \f$ P'_0(x) = 0 \f$ and \f$ P'_1(x) = 1 \f$.
      *
+     * It throws a simplemc::simplemc_exception if \f$ |x| > 1 \f$.
+     *
      * @param x Value at which the polynomials and its derivatives are evaluated.
      */
-    legendre_polynomial(double x = 0.0);
+    constexpr legendre_polynomial(double x = 0.0) : x_(x) {
+        if (std::abs(x_) > 1) {
+            throw simplemc_exception("Legendre polynomials are only defined on the interval [-1, 1]");
+        }
+    }
 
     /**
      * @brief Get the order \f$ l \f$ of the currently processed polynomial \f$ P_l(x) \f$.
      *
      * @return Order \f$ l \f$.
      */
-    [[nodiscard]] int order() const { return l_; }
+    [[nodiscard]] constexpr int order() const noexcept { return l_; }
 
     /**
      * @brief Get the \f$ x \f$ value at which the polynomials are evaluated.
      *
      * @return \f$ x \f$ value given at construction.
      */
-    [[nodiscard]] double x() const { return x_; }
+    [[nodiscard]] constexpr double x() const noexcept { return x_; }
 
     /**
      * @brief Get the function value of the currently processed polynomial.
      *
      * @return \f$ P_l(x) \f$.
      */
-    [[nodiscard]] double current() const { return pl_; }
+    [[nodiscard]] constexpr double current() const noexcept { return pl_; }
 
     /**
      * @brief Get the function value of the previously processed polynomial.
      *
+     * @note The behaviour is undefined for \f$ l = 0 \f$.
+     *
      * @return \f$ P_{l-1}(x) \f$.
      */
-    [[nodiscard]] double prev() const {
-        assert(l_ > 0);
-        return plm1_;
-    }
+    [[nodiscard]] constexpr double prev() const noexcept { return plm1_; }
 
     /**
      * @brief Get the derivative of the currently processed polynomial.
      *
      * @return \f$ P'_l(x) \f$.
      */
-    [[nodiscard]] double current_derivative() const { return dpl_; };
+    [[nodiscard]] constexpr double current_derivative() const noexcept { return dpl_; }
 
     /**
      * @brief Get the derivative of the previously processed polynomial.
      *
+     * @note The behaviour is undefined for \f$ l = 0 \f$.
+     *
      * @return \f$ P'_{l-1}(x) \f$.
      */
-    [[nodiscard]] double prev_derivative() const {
-        assert(l_ > 0);
-        return dplm1_;
-    };
+    [[nodiscard]] constexpr double prev_derivative() const noexcept { return dplm1_; }
 
     /**
      * @brief Increase the order from \f$ l \f$ to \f$ l + 1 \f$.
@@ -144,15 +148,28 @@ public:
      *
      * @return Function value of the polynomial \f$ P_l(x) \f$.
      */
-    double next();
+    constexpr double next() noexcept {
+        double plp1 = x_;
+        double dplp1 = 1;
+        if (l_ > 0) {
+            plp1 = ((2 * l_ + 1) * x_ * pl_ - l_ * plm1_) / (l_ + 1);
+            dplp1 = (l_ + 1) * pl_ + x_ * dpl_;
+        }
+        plm1_ = pl_;
+        pl_ = plp1;
+        dplm1_ = dpl_;
+        dpl_ = dplp1;
+        ++l_;
+        return plm1_;
+    }
 
 private:
     int l_ { 0 };
     double x_;
-    double plm1_;
-    double pl_;
-    double dplm1_;
-    double dpl_;
+    double plm1_ { 0.0 };
+    double pl_ { 1.0 };
+    double dplm1_ { 0.0 };
+    double dpl_ { 0.0 };
 };
 
 /** @} */
